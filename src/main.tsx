a8419c778c23fe2c179598133ce3e3f802318422
oldsheeep3 <js2oyb@gmail.com>
2026-04-18 15:32:13 +0000

    feat: integrate authentication with AuthProvider and AuthGuard
    - Removed unused imports and components from App.tsx
    - Added user information display and logout functionality in App.tsx
    - Set up AuthProvider and AuthGuard in main.tsx for authentication context
    - Created symlink for auth-component to facilitate imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { AuthProvider, AuthGuard } from './auth-component/src';
 import App from './App';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider
+      config={{
+        baseURL: 'https://auth.sheeplab.net',
+      }}
+    >
+      <AuthGuard>
+        <App />
+      </AuthGuard>
+    </AuthProvider>
   </StrictMode>,
 );
